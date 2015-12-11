f995c44edb40556a137cfc03876fd4b749dc599d
Yaroslav Halchenko <debian@onerussian.com>
2015-12-11 18:20:50 -0500

    trying to create .cue files

--- .datalad/crawl/pipelines/pipeline.py
+++ .datalad/crawl/pipelines/pipeline.py
@@ -2,7 +2,9 @@
 Pipeline to fetch/update ratholeradio shows
 """
 from __future__ import print_function
+import re
 
+from datalad.utils import updated
 from datalad.crawler.nodes.annex import Annexificator
 from datalad.crawler.nodes.crawl_url import crawl_url
 from datalad.crawler.nodes.matches import a_href_match, css_match
@@ -11,16 +13,76 @@ from logging import getLogger
 lgr = getLogger('datalad.custom.ratholeradio')
 
 def process_episode(data):
-    print("URL: {url}".format(**data))
-    import pdb; pdb.set_trace()
-    i = 1
-#    yield data
+    # create cue out of
+    lgr.info("Processing URL: {url}".format(**data))
+    items = []
+    # some times \u2014 was used to split up pairs
+    for item in data['items']:
+        items.extend(item.split(u'\u2014'))
+
+    tracks = []
+    for item in items:
+        fields = [re.sub('<.*?>', '', x).strip()  # strip all HTML goodness
+                  for x in item.split(u'\u2013')]
+        fields = filter(bool, fields)  # remove empty
+        if len(fields) > 0:
+            if len(fields) == 4:
+                time, artist, title, license = fields
+            elif len(fields) == 3:
+                time, artist, title = fields
+                license = None
+            else:
+                lgr.warning("Got %d fields: %s" % (len(fields), str(fields)))
+                continue
+            # Let's just write that time
+            try:
+                _ = map(int, time.split(':', 1))
+            except:
+                # must be smth else but time
+                continue
+        else:
+            continue
+        tracks.append({'time': time, 'artist': artist, 'title': title, 'license': license})
+
+    if tracks:
+        mp3_file = data['mp3'][0].split('/')[-1]
+        assert(mp3_file.endswith('.mp3'))
+        lgr.info("Harvested %d tracks for the %s" % (len(tracks), mp3_file))
+        for ext in ('mp3', 'ogg'):
+            ext_file = mp3_file.replace('.mp3', '.' + ext)
+            cue_file = mp3_file.replace('.mp3', '_%s.cue' % ext)
+            lgr.debug("Composing %s", cue_file)
+            data_ = data.copy()
+            data_['EXT'] = ext.upper()
+            data_['ext_file'] = ext_file
+            with open(cue_file, 'w') as f:
+                f.write("""REM GENRE "Eclectic music from around the web"
+REM DATE "{year}"
+PERFORMER "Dan Lynch"
+TITLE "Episode {episode}"
+FILE "{ext_file}" {EXT}
+TRACK 01 AUDIO
+ TITLE "Introduction"
+ PERFORMER "Dan Lynch"
+ INDEX 01 00:00:00
+""".format(**data_))
+                for i, track in enumerate(tracks, 2):
+                    f.write("""\
+TRACK {index:02d} AUDIO
+ TITLE "{title}"
+ PERFORMER "{artist}"
+ INDEX 01 {time}:00
+""".format(index=i, **track))
+            out = updated(data, dict(filename=cue_file))
+            out.pop('url')  # URL is no longer associated with this file
+            yield out
 
 def pipeline():
     lgr.info("Creating a pipeline for the ratholeradio")
     annex = Annexificator(
         create=False,  # must be already initialized etc
-        options=["-c", "annex.largefiles=exclude=*.txt and exclude=*.cue"])
+        mode='relaxed',
+        options=["-c", "annex.largefiles=exclude=*.cue"])
 
     return [
         crawl_url("http://ratholeradio.org",
@@ -30,11 +92,23 @@ def pipeline():
         a_href_match(".*/(?P<year>2[0-9]{3})/(?P<month>[0-9]{1,2})/ep(?P<episode>[0-9]+)/?$"),
         crawl_url(),
         # a_href_match(".*/RR.*\.(ogg|mp3)"),  # then would be one per each file
-        css_match('div#page .entry',
-                  xpaths={
-                      'items': '//p',
-                      'links': '//a',
-                  },
-                  allow_multiple=True),
-        process_episode
+        # css_match('div#page .entry',
+        #           xpaths={
+        #               'items': '//p',
+        #               'links': '//a',
+        #               'mp3_urls': "//a[contains(@href, '.mp3')]//@href"
+        #           },
+        #           allow_multiple=True),
+        [
+            css_match('div#page .entry',
+                      xpaths={'items': '//p',
+                              'mp3': "//a[contains(@href, '.mp3')]//@href"},
+                      allow_multiple=True),
+            process_episode,
+            annex
+        ],
+        [
+            a_href_match('.*/RR.*\.(ogg|mp3)'),
+            annex
+        ]
     ]
